c020332f81bbd6c2f35c3cfa8e9d46b298164a8f
Emma Thulliez <emma.thulliez@insa-rouen.fr>
2026-02-05 16:58:42 +0100

    deleted ejs

--- header.js
+++ header.js
@@ -1,12 +1,7 @@
-console.log("ok")
-
-const head = `<!--<img src="Images/ethulliez.PNG" alt="Photo de Emma Thulliez" style="float:right;width:200px;">-->
-  </br>
-  <name>Emma Thulliez</name>
+const head = `<name>Emma Thulliez</name>
   <p>Chargée d'Enseignement Contractuelle | AgroParisTech, Palaiseau, France
   </br> <i class="fa-regular fa-envelope"></i> emma.thulliez@agroparistech.com
   </br><i class="fa-brands fa-orcid"></i> <a href="https://orcid.org/0009-0000-5679-3036" target="_blank" style="color: white;">Emma Thulliez</a>
-  </p>
-  </br>`
+  </p>`
   
 document.getElementsByTagName("header")[0].innerHTML = head

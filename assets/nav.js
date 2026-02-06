ae05160de68810df3750ada7af2d9ab1b846ac26
Emma Thulliez <emma.thulliez@insa-rouen.fr>
2026-02-06 17:25:14 +0100

    Two-column style + talks

--- assets/nav.js
+++ assets/nav.js
@@ -1,8 +1,9 @@
 const navbar = `
-    <div id="navbar"><a href="index.html#apropos">À propos</a>
+    <div id="navbar"><a href="#apropos">À propos</a>
     <a href="index.html#situation">Situation</a>
     <a href="publications.html">Publications</a>
+    <a href="talks.html">Talks</a>
     <a href="enseignements.html">Enseignements</a>
-    <a href="index.html#contact">Contact</a></div>`
+    <a href="#contact">Contact</a></div>`
   
 document.getElementsByTagName("nav")[0].innerHTML = navbar
---
+++ left.js
@@ -0,0 +1,18 @@
+const leftcol = `<div id="apropos">
+    <h2>À propos</h2>
+    <p>Docteure en statistiques appliquées à l'environnement, 
+    je m'intéresse particulièrement aux sujets liés à la pollution de l'air. 
+    Mes travaux de recherche se concentrent sur l'emploi des micro-capteurs 
+    pour améliorer la surveillance de la pollution en milieu urbain. 
+    D'une part, il s'agit de calibrer les micro-capteurs afin d'en obtenir des 
+    mesures de concentrations précises, et d'autre part, de les employer afin
+    d'améliorer la cartographie des polluants.</p>
+  </div>
+  
+  <div id="contact">
+    <h2>Contact</h2>
+    <p> <i class="fa-regular fa-envelope"></i> emma.thulliez@agroparistech.com</p>
+    <p><i class="fa-brands fa-linkedin"></i> <a href="https://fr.linkedin.com/in/emma-thulliez" target="_blank">Emma Thulliez</a></p>
+  </div>`
+  
+document.getElementsByTagName("left")[0].innerHTML = leftcol

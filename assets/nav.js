b8840569fd57e9457be2a368e9677a75b8f02a86
Emma Thulliez <emma.thulliez@insa-rouen.fr>
2026-02-06 18:50:30 +0100

    Update nav.js

--- assets/nav.js
+++ assets/nav.js
@@ -1,10 +1,9 @@
 const navbar = `
-    <div id="navbar"><a href="#apropos">À propos</a>
+    <div id="navbar">
     <a href="index.html#situation">Situation</a>
     <a href="publications.html">Publications</a>
     <a href="talks.html">Talks</a>
     <a href="enseignements.html">Enseignements</a>
-    <a href="data.html">Open Data</a>
-    <a href="#contact">Contact</a></div>`
+    <a href="data.html">Open Data</a></div>`
   
 document.getElementsByTagName("nav")[0].innerHTML = navbar

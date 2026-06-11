8f18daf65aa2f31218a9488c37885f264a52d59c
Emma Thulliez <emma.thulliez@insa-rouen.fr>
2026-06-11 10:35:13 +0200

    Updated links

--- assets/header.js
+++ assets/header.js
@@ -1,7 +1,7 @@
 const head = `<p><i style="font-size: 18px">Chargée d'Enseignement Contractuelle</i>
   </br><i>AgroParisTech, Palaiseau, France</i></p>
   <p>
-  </br> emma.thulliez@agroparistech.fr <i class="fa-regular fa-envelope"></i>
+  </br> <a href="mailto:emma.thulliez@agroparistech.fr" style="color: white;">emma.thulliez@agroparistech.fr</a> <i class="fa-regular fa-envelope"></i>
   </p><p style="font-size:25px"><a href="https://orcid.org/0009-0000-5679-3036" target="_blank" style="color: white;"><i class="fa-brands fa-orcid"></i></a>
   <a href="https://github.com/ethulliez" target="_blank" style="color: white;"><i class="fa-brands fa-github"></i></a>
   <a href="https://scholar.google.com/citations?hl=fr&user=tW6kUPcAAAAJ" target="_blank" style="color: white;"><i class="fa-brands fa-google-scholar"></i></a>

993a6572c7e795573fc946059b50cdaef72d4e81
Emma Thulliez <emma.thulliez@insa-rouen.fr>
2026-09-04 11:01:02 +0200

    Corrected links

---
+++ nav.js
@@ -0,0 +1,23 @@
+function languageFunction(){
+  //console.log("Language:", document.documentElement.lang);
+  if(document.documentElement.lang === "fr"){
+    document.documentElement.lang = "en"
+    window.location.href = "https://ethulliez.github.io/en/index.html";
+  } else {
+    document.documentElement.lang = "fr"
+    window.location.href = "https://ethulliez.github.io/fr/index.html";
+  }
+  //console.log("Changed language to:", document.documentElement.lang);
+}
+
+const navbar = `
+    <div id="navbar">
+    <a href="fr/index.html">Situation</a>
+    <a href="fr/publications.html">Publications</a>
+    <a href="fr/talks.html">Talks</a>
+    <a href="fr/enseignements.html">Enseignements</a>
+    <a href="fr/data.html">Open Data</a>
+    <button value = "change_language" onclick="languageFunction();"><i class="fa-solid fa-language"></i></button>
+    </div>`
+  
+document.getElementsByTagName("nav")[0].innerHTML = navbar
